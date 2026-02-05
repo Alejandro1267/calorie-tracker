6b7e291719534910f5da831df21adb7b52ac2abb
Alejandro1267 <164577387+Alejandro1267@users.noreply.github.com>
2026-02-05 13:42:45 -0700

    Add ContextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import Form from "./components/Form"
-import { useReducer, useEffect, useMemo } from "react"
-import { activityReducer, initialState } from "./reducers/activity-reducer"
-import ActivityList from "./components/ActivityList"
-import CalorieTracker from "./components/CalorieTracker"
+import Form from "./components/Form";
+import { useEffect, useMemo } from "react";
+import ActivityList from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
+import { useActivity } from "./hooks/useActivity";
 
 function App() {
-
-  const [state, dispatch] = useReducer(activityReducer, initialState)
+  const { state, dispatch } = useActivity();
 
   useEffect(() => {
-    localStorage.setItem("activities", JSON.stringify(state.activities))
-  }, [state.activities])
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
 
-  const canRestartApp = () => useMemo(() => state.activities.length > 0, [state.activities])
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length > 0, [state.activities]);
 
   return (
     <>
@@ -21,38 +21,30 @@ function App() {
           <h1 className="text-center text-lg font-bold text-white">
             Contador de Calorías
           </h1>
-          <button 
+          <button
             className="bg-gray-800 hover:bg-gray-900 p-2 font-bold text-white cursor-pointer rounded-lg text-sm disabled:opacity-40"
             disabled={!canRestartApp()}
-            onClick={() => dispatch({type: "restart-app"})}
-            >
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
             Reiniciar App
           </button>
         </div>
       </header>
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form
-            dispatch={dispatch}
-            state={state}
-          />
+          <Form />
         </div>
       </section>
       <section className="bg-gray-800 py-10">
         <div className="max-w-4xl mx-auto">
-          <CalorieTracker 
-            activities={state.activities}
-          />
+          <CalorieTracker />
         </div>
       </section>
       <section className="p-10 mx-auto max-w-4xl">
-        <ActivityList
-          activities={state.activities}
-          dispatch={dispatch}
-        />
+        <ActivityList />
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
